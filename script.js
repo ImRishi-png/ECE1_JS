8d2bce5d34169133938d43fa863b4cb32296e42b
ImRishi-png <rishic0099@gmail.com>
2026-03-11 10:30:05 +0530

    Functions

--- script.js
+++ script.js
@@ -46,24 +46,49 @@
 // else{
 //     document.write("<b>You are not eligible to cast the vote.</b>");
 // }   
-     var grade='C';
-     document.write("Entering switch block<br />");
-     switch(grade){
-        case 'A':
-            document.write("Good job<br />");
-            break;      
-        case 'B':
-            document.write("Pretty good<br />");
-            break;
-        case 'C':
-            document.write("Passed<br />");
-            break;
-        case 'D':   
-            document.write("Not so good<br />");
-            break;
-        case 'F':
-            document.write("Failed<br />");
-            break;
-        default:
-            document.write("Invalid grade!<br />");
-     }
+    //  var grade='C';
+    //  document.write("Entering switch block<br />");
+    //  switch(grade){
+    //     case 'A':
+    //         document.write("Good job<br />");
+    //         break;      
+    //     case 'B':
+    //         document.write("Pretty good<br />");
+    //         break;
+    //     case 'C':
+    //         document.write("Passed<br />");
+    //         break;
+    //     case 'D':   
+    //         document.write("Not so good<br />");
+    //         break;
+    //     case 'F':
+    //         document.write("Failed<br />");
+    //         break;
+    //     default:
+    //         document.write("Invalid grade!<br />");
+    //  }
+// function myFunction(){
+//     alert("Hello World");
+// }
+// function sayHello(name,age){
+//     document.write(name+" is "+age+" years old.");
+// }
+var person={
+    name:'John Mac',
+    age:30,
+    isMarried:true,
+    address:{
+        street:'vinay nagar'
+    }
+};
+console.log(person);
+var person2= new Object();
+person2.name='Krishna';
+person2.age=30;
+person2.address={};
+person2.address.street='nagar road';
+console.log(person2);
+// difference b/w object and array 
+// array is ordered collection of data and object is collection of key value pairs
+// array can be accessed by index and object can be accessed by key
+// array can contain any type of data and object can contain any type of data
