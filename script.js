f52a14fa9bed55b656e4095792518930273db252
ImRishi-png <rishic0099@gmail.com>
2026-03-11 09:44:04 +0530

    Loops and Condition Statement

--- script.js
+++ script.js
@@ -14,13 +14,56 @@
 // num;
 // console.log(num);
 // console.log(typeof(num));
-const sym1= Symbol(4)
-console.log(sym1);
-const sym2 =Symbol(4);
-console.log(sym2);
-if(sym1===sym2){
-    console.log("true");
-}
-else{
-    console.log("false");
-}
+// const sym1= Symbol(4)
+// console.log(sym1);
+// const sym2 =Symbol(4);
+// console.log(sym2);
+// if(sym1===sym2){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+// var count
+// document.write("Starting Loop "+ "<br>");
+// for(count=0;count<10;count++) {
+//     document.write("Current count:"+count);
+//     document.write("<br/>");
+// }
+// document.write("Loop stopped!");
+// const person={
+//     fname:"Ajay",
+//     lname:"Singh",
+//     age:25,
+// };
+// for(let x in person){
+//     console.log("person details:"+x+":"+person[x]);
+// }
+// var age = 20;
+// if(age>18){
+//     document.write("<b>You are eligible to cast the vote.</b>");
+// }
+// else{
+//     document.write("<b>You are not eligible to cast the vote.</b>");
+// }   
+     var grade='C';
+     document.write("Entering switch block<br />");
+     switch(grade){
+        case 'A':
+            document.write("Good job<br />");
+            break;      
+        case 'B':
+            document.write("Pretty good<br />");
+            break;
+        case 'C':
+            document.write("Passed<br />");
+            break;
+        case 'D':   
+            document.write("Not so good<br />");
+            break;
+        case 'F':
+            document.write("Failed<br />");
+            break;
+        default:
+            document.write("Invalid grade!<br />");
+     }
