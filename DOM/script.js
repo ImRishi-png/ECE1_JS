0dd6dcdfc3a0900f7967f7160a4ec0939d2ab2f6
ImRishi-png <rishic0099@gmail.com>
2026-03-19 11:34:51 +0530

    Event

---
+++ DOM/script.js
@@ -0,0 +1,27 @@
+// const result=document.getElementById("jk");
+// console.log(result);
+// result.innerHTML="<b>Hello World</b>";
+// const result1=document.getElementsByClassName("p1");
+// console.log(result1);
+// result1[1].innerHTML="chauhan";
+// result1[0].innerHTML="Jitendra";
+// const result=document.getElementById("jk").getAttribute("class");
+// const x=document.getElementById("jk").innerHTML="result";
+function onClick(){
+    console.log("Clicked");
+}
+    function onMouseOut(){
+        console.log("on Mouse Out")
+}
+function onMouseDown(){
+    console.log("on Mouse Down")
+}
+function onMouseUp(){
+    console.log("onMouse Move")
+}
+function onMouseOver() {
+    console.log("on Mouse Over")
+}
+function ondblclick(){
+    console.log("on Db Click")
+}
